4c64cbd70f6f0a524d56463369563a555623345f
unknown <https://github.com/Vaibhav291/Asp.Net-Core.git>
2023-12-16 23:58:24 +0530

    Repository Created

---
+++ Code to Build/Code to Build/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Code_to_Build
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}

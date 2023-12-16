68e68c1cf26070fac2849debada8b8fe5f21b473
unknown <https://github.com/Vaibhav291/Asp.Net-Core.git>
2023-12-17 03:47:04 +0530

    ANCBE-3 : Updated changes

---
+++ Code to Build/Code to Build/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using Code_to_Build.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Code_to_Build.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : Controller
+    {
+        private readonly CodetoBuildDbContext _context;
+
+        public RegistrationController(CodetoBuildDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Index([FromBody] User registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.users.Add(registration);
+            await _context.SaveChangesAsync();
+
+            return Ok("Registration Successfull");
+        }
+    }
+}    
